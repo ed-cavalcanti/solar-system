606e5cfcc646d534915e57a2bfac2da301e44682
ed-cavalcanti <edney2809@gmail.com>
2022-04-06 16:13:35 -0300

    06 - render planets with PlanetCards component

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const cards = Planets.map((planet, i) => {
+      const { name, image } = planet;
+      return <PlanetCard key={ i } planetName={ name } planetImage={ image } />;
+    });
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { cards }
       </div>
     );
   }
